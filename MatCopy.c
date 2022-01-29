d6c4513a7ae0f0b9236b72546aaf127d18a5da89
Mickael Cottin Bizonne <mickael.cottin.bizonne@gmail.com>
2022-01-29 23:07:35 +0000

    FIX - Modify destination address in FC L1 memory
    Description: Todo > What is the right memory destination : CL or FC ?

--- MatCopy.c
+++ MatCopy.c
@@ -5,7 +5,8 @@
 #define MATRIX_HEIGHT 	(64)
 #define MATRIX_SIZE		(MATRIX_WIDTH * MATRIX_HEIGHT)
 
-static unsigned short *p_mat1_l2_buf, *p_mat2_l2_buf, *p_mat1_cl_l1_buf, *p_mat2_cl_l1_buf;
+static unsigned short *p_mat1_l2_buf, *p_mat2_l2_buf, *p_mat1_fc_l1_buf, *p_mat2_fc_l1_buf;
+static struct pi_device dmacpy;
 
 void mainController(void)
 {
@@ -13,6 +14,7 @@ void mainController(void)
 	struct pi_cluster_conf 	cluster_conf;
 	struct pi_device 		device; // global ? 
 	struct pi_cluster_task 	cluster_task;
+    struct pi_dmacpy_conf 	dmacpy_conf;
 
 	// Memory allocation
 
@@ -32,22 +34,31 @@ void mainController(void)
         pmsis_exit(-1);
     }
 
-    // Matrix one allocation in CL L1
-	p_mat1_cl_l1_buf = (unsigned short*)pi_cl_l1_malloc(&device,(uint32_t)MATRIX_SIZE);
-    if(p_mat1_cl_l1_buf == NULL)
+    // Matrix one allocation in FC L1
+	p_mat1_fc_l1_buf = (unsigned short*)pi_fc_l1_malloc(MATRIX_SIZE);
+    if(p_mat1_fc_l1_buf == NULL)
     {
-        printf("Failed to allocate Matrix1 in CL L1 !\n");
+        printf("Failed to allocate Matrix1 in FC L1 !\n");
         pmsis_exit(-1);
     }
 
     // Matrix two allocation in L2
-	p_mat2_cl_l1_buf = (unsigned short*)pi_cl_l1_malloc(&device, (uint32_t)MATRIX_SIZE);
-    if(p_mat2_cl_l1_buf == NULL)
+	p_mat2_cl_l1_buf = (unsigned short*)pi_fc_l1_malloc(MATRIX_SIZE);
+    if(p_mat2_fc_l1_buf == NULL)
     {
         printf("Failed to allocate Matrix2 in CL L1!\n");
         pmsis_exit(-1);
     }
 
+    // FILL BUFFERS 
+
+    for(int id = 0; id < MATRIX_SIZE; id++)
+    {
+    	p_mat1_l2_buf[id] = 2;
+    	p_mat2_l2_buf[id] = 3;
+    	p_mat1_fc_l1_buf[id] = 0;
+    	p_mat2_fc_l2_buf[id] = 0;
+    }
 
     pmsis_exit(errors);
 }
