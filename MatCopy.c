b88495998b3d1d2ff23410da99e526df4db161db
Mickael Cottin Bizonne <mickael.cottin.bizonne@gmail.com>
2022-01-29 23:13:03 +0000

    DOC - STYLE: Add comments on buffer & removed unused variables

--- MatCopy.c
+++ MatCopy.c
@@ -5,16 +5,19 @@
 #define MATRIX_HEIGHT 	(64)
 #define MATRIX_SIZE		(MATRIX_WIDTH * MATRIX_HEIGHT)
 
-static unsigned short *p_mat1_l2_buf, *p_mat2_l2_buf, *p_mat1_fc_l1_buf, *p_mat2_fc_l1_buf;
+static unsigned short *p_mat1_l2_buf; 		// Matrix1 Buffer pointer in L2 memory
+static unsigned short *p_mat2_l2_buf; 		// Matrix2 Buffer pointer in L2 memory
+static unsigned short *p_mat1_fc_l1_buf; 	// Matrix1 Buffer pointer in FC L1 memory
+static unsigned short *p_mat2_fc_l1_buf; 	// Matrix2 Buffer pointer in FC L1 memory
 static struct pi_device dmacpy;
 
 void mainController(void)
 {
 	int errors = 0;
-	struct pi_cluster_conf 	cluster_conf;
-	struct pi_device 		device; // global ? 
-	struct pi_cluster_task 	cluster_task;
     struct pi_dmacpy_conf 	dmacpy_conf;
+    pi_task_t task_copy_mat1_l2_fcl1; // Task dedicated to copying Matrix1 from L2 to L1
+    pi_task_t task_copy_mat2_l2_fcl1; // Task dedicated to copying Matrix2 from L2 to L1	
+	dma_memcpy_t
 
 	// Memory allocation
 
@@ -52,6 +55,15 @@ void mainController(void)
 
     // FILL BUFFERS 
 
+    pi_dmacpy_conf_init(&dmacpy_conf);
+    pi_open_from_conf(&dmacpy, &dmacpy_conf);
+    errors = pi_dmacpy_open(&dmacpy);
+    if (errors)
+    {
+        printf("Error dmacpy open : %ld !\n", errors);
+        pmsis_exit(-3);
+    }
+
     for(int id = 0; id < MATRIX_SIZE; id++)
     {
     	p_mat1_l2_buf[id] = 2;
@@ -59,6 +71,8 @@ void mainController(void)
     	p_mat1_fc_l1_buf[id] = 0;
     	p_mat2_fc_l2_buf[id] = 0;
     }
+
+
 
     pmsis_exit(errors);
 }
